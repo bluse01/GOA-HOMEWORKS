cdab6a6c6fd3814af7c4fb06652fb4b5683a0a4a
bluse01 <zuraguruli07@gmail.com>
2024-05-26 17:19:15 +0400

    Hello World

--- Day 00/homework/lesson00.py
+++ Day 00/homework/lesson00.py
@@ -1,53 +1,66 @@
 from turtle import *
 
-# box
-width(5)
-color("black")
-for i in range(4):
-    forward(200)
+def draw_square(x, y, penwidth, pencolor):
+    pensize(penwidth)
+    color(pencolor)
+    penup()
+    setx(x)
+    sety(y)
+    pendown()
+    for _ in range(4):
+        forward(200)
+        left(90)
+
+def draw_door(x, y, penwidth, pencolor):
+    pensize(penwidth)
+    color(pencolor)
+    penup()
+    setx(x)
+    sety(y)
+    pendown()
+
+    begin_fill()
     left(90)
+    forward(100)
+    right(90)
+    forward(60)
+    right(90)
+    forward(100)
+    end_fill()
 
-# door
-color("gray")
-penup()
-goto(70, 0)
-pendown()
+def draw_roof(x, y, penwidth, pencolor):
+    pensize(penwidth)
+    color(pencolor)
+    penup()
+    setx(x)
+    sety(y)
+    pendown()
 
-left(90)
-forward(100)
-right(90)
-forward(60)
-right(90)
-forward(100)
+    begin_fill()
+    left(45)
+    forward(-142)
+    left(90)
+    forward(-140)
+    end_fill()
 
-# roof
-color("black")
-penup()
-goto(200, 200)
-pendown()
+def draw_window(x, y, penwidth, pencolor):
+    pensize(penwidth)
+    color(pencolor)
+    penup()
+    setx(x)
+    sety(y)
+    pendown()
 
+    for _ in range(4):
+        forward(50)
+        left(90)
+
+
+draw_square(0, 0, 5, "black")
+draw_door(70, 0, 5, "black")
+draw_roof(200, 200, 5, "black")
 left(45)
-forward(-143)
-left(90)
-forward(-140)
-
-# windows
-color("orange")
-penup()
-goto(175, 125)
-pendown()
-
-left(45)
-for i in range(4):
-    forward(50)
-    left(90)
-
-penup()
-goto(75, 125)
-pendown()
-
-for i in range(4):
-    forward(50)
-    left(90)
+draw_window(175, 125, 5 , "black")
+draw_window(75, 125, 5 , "black")
 
 exitonclick()
