237cca58d08136ec7733186a4287bdb1553e3842
bluse01 <zuraguruli07@gmail.com>
2024-08-11 15:42:42 +0400

    upload

---
+++ Day 26/HomeWork/homework2.py
@@ -0,0 +1,17 @@
+
+my_list = [2, 3, 5] 
+
+# append a num
+
+my_list.append(1) # adds val to last index
+print(f".append {my_list}")
+
+# pop
+
+my_list.pop(2) # removes 5 becuase it on 2 position
+print(f".pop {my_list}")
+
+# insert
+
+my_list.insert(1 , 9) # index 1 adds int 9
+print(f".isert {my_list}")
---
+++ Day 26/HomeWork/homeworl.py
@@ -0,0 +1,15 @@
+
+# Upper
+my_string1 = "a string"
+
+print(my_string1.upper())
+
+# Lower
+my_string2 = "a lower string"
+
+print(my_string2.lower())
+
+# Cap
+my_string3 = "Cap string"
+
+print(my_string3.capitalize())
